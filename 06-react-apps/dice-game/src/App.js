2ee7f7adef797a3904bbdc43867fa55094336db8
Hanut Singh Gusain <hanutsingh@gmail.com>
2021-11-17 22:48:55 +0530

    updated stuff

--- 06-react-apps/dice-game/src/App.js
+++ 06-react-apps/dice-game/src/App.js
@@ -1,20 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 import rollDice from "./libs/roll-dice";
 
 function TitleComponent() {
   return <h1 className="title">Dice Game</h1>;
 }
 
-function Dice() {
-  const [imageSrc, setImageSrc] = useState(rollDice());
+function Dice({ id }) {
+  const [imgSrc, setImgSrc] = useState(rollDice());
+
+  const _handleOnClick = () => {
+    setImgSrc(rollDice());
+  };
+
   return (
-    <div class="col">
-      <img
-        id="dice1"
-        src={imageSrc}
-        alt="dice one"
-        onClick={() => setImageSrc(rollDice())}
-      />
+    <div className="col">
+      <img id={id} src={imgSrc} alt={imgSrc} onClick={_handleOnClick} />
     </div>
   );
 }
@@ -24,8 +24,8 @@ function App() {
     <div className="container">
       <TitleComponent />
       <div className="row">
-        <Dice />
-        <Dice />
+        <Dice id="dice1"/>
+        <Dice id="dice2"/>
       </div>
       <button id="rollBtn">Roll All</button>
     </div>
